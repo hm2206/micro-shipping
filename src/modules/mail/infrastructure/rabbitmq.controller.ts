8ee16184743d9c562303b68130e34d13dc512551
Hans <twd2206@gmail.com>
2021-12-04 16:07:25 -0500

    fix datos

--- src/modules/mail/infrastructure/rabbitmq.controller.ts
+++ src/modules/mail/infrastructure/rabbitmq.controller.ts
@@ -43,7 +43,7 @@ export class RabbitMqController {
             subscriber.next(data);
             resetTimeout(controlTime);
             // enviar email procesado
-            sendMailProcess
+            sendMailProcess();
           },
           error: (err) => {
             subscriber.error(err);
