96f0f504e13a49981ce9d4fc39759c3f3792a18b
hans medina <twd2206@gmail.com>
2022-01-25 12:35:44 -0500

    fix microservice

--- src/microservices.ts
+++ src/microservices.ts
@@ -1,11 +1,11 @@
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 const { RABBITMQ_USER, RABBITMQ_PASS, RABBITMQ_HOST, RABBITMQ_PORT, RABBITMQ_VIRTUAL } = process.env;
 
-export const SENT_MAIL = {
+export const SENT_MAIL: MicroserviceOptions = {
   transport: Transport.RMQ,
   options: {
     urls: [`amqp://${RABBITMQ_USER}:${RABBITMQ_PASS}@${RABBITMQ_HOST}:${RABBITMQ_PORT}/${RABBITMQ_VIRTUAL}`],
     queue: 'shipping',
     noAck: false
   }
-} as MicroserviceOptions
+}
